b7a764ce8f6b6d2fb0b8104fcfef34db9a00914d
evgeniyamiller <evgeniya_hvorostova@mail.ru>
2022-08-10 01:30:35 +0200

    1,2,3 задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+            System.out.println("Задание 1");
+
+    int i = 1;
+    while (i<=10){
+        System.out.print(i + " ");
+    i++;
+}
+        System.out.println();
+
+
+    for ( int a = 10; a>=1; a--)
+    {
+        System.out.print(a + " ");
+    }
+
+        //Задание 2
+        System.out.println();
+        System.out.println("Задание 2");
+
+        for (int firstFriday = 5; firstFriday <= 31; firstFriday = firstFriday + 7)
+        {
+            System.out.println("Сегодня пятница, " + firstFriday+ "-е число. Необходимо подготовить отчет.");
+        }
+
+        //Задание 3
+        System.out.println("Задание 3");
+
+        int aCometIsFlying = 0;
+        int year = 2022;
+        int yearAgo200 = year - 200;
+        int yearAfter100 = year + 100;
+        for (; aCometIsFlying<=2122; aCometIsFlying = aCometIsFlying + 79) {
+            if (aCometIsFlying > yearAgo200 && aCometIsFlying < yearAfter100) {
+
+                System.out.println(aCometIsFlying);
+
+            }
+        }
+
+        ;
     }
 }
